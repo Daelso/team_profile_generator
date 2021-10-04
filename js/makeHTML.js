4f35cd3af818142a6875d0dd6137143b48166098
Chase Murtaugh <cmurtau@g.clemson.edu>
2021-10-04 19:35:03 -0400

    CSS and bootstrap styling

--- js/makeHTML.js
+++ js/makeHTML.js
@@ -2,15 +2,15 @@
 const generateManager = function (manager) {
     return `
     <div class="col-4 mt-4">
-        <div class="card h-100">
-            <div class="card-header">
+    <div style="border-radius: 25px;" class="card h-100 shadow-lg">
+        <div class="card-header bg-primary text-white">
                 <h3>${manager.name}</h3>
                 <h4>Manager</h4><i class="material-icons">content_paste</i>
             </div>
             <div class="card-body">
-                <p class="id">ID: ${manager.id}</p>
-                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-                <p class="office">Office Number: ${manager.officeNumber}</p>
+                <p style="border: 1px solid black; border-radius: 5px; padding: 5px;" class="id">ID: ${manager.id}</p>
+                <p style="border: 1px solid black; border-radius: 5px; padding: 5px;" class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p style="border: 1px solid black; border-radius: 5px; padding: 5px;" class="office">Office Number: ${manager.officeNumber}</p>
             </div>
         </div>
     </div>
@@ -21,15 +21,15 @@ const generateManager = function (manager) {
 const generateEngineer = function (engineer) {
     return `
     <div class="col-4 mt-4">
-        <div class="card h-100">
-            <div class="card-header">
+    <div style="border-radius: 25px;" class="card h-100 shadow-lg">
+        <div class="card-header bg-primary text-white">
                 <h3>${engineer.name}</h3>
                 <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
             </div>
             <div class="card-body">
-                <p class="id">ID: ${engineer.id}</p>
-                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
-                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+                <p style="border: 1px solid black; border-radius: 5px; padding: 5px;" class="id">ID: ${engineer.id}</p>
+                <p style="border: 1px solid black; border-radius: 5px; padding: 5px;" class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p style="border: 1px solid black; border-radius: 5px; padding: 5px;" class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
             </div>
         </div>
     </div>
@@ -40,15 +40,15 @@ const generateEngineer = function (engineer) {
 const generateIntern = function (intern) {
     return `
     <div class="col-4 mt-4">
-        <div class="card h-100">
-            <div class="card-header">
+    <div style="border-radius: 25px;" class="card h-100 shadow-lg">
+        <div class="card-header bg-primary text-white">
                 <h3>${intern.name}</h3>
                 <h4>Intern</h4><i class="material-icons">assignment_ind</i>
             </div>
             <div class="card-body">
-                <p class="id">ID: ${intern.id}</p>
-                <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
-                <p class="school">School: ${intern.school}</p>
+                <p style="border: 1px solid black; border-radius: 5px; padding: 5px;" class="id">ID: ${intern.id}</p>
+                <p style="border: 1px solid black; border-radius: 5px; padding: 5px;" class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p style="border: 1px solid black; border-radius: 5px; padding: 5px;" class="school">School: ${intern.school}</p>
             </div>
     </div>
 </div>
